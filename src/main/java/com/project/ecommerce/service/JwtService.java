e107feb6e318ea590c21f71368776ea325c02c65
SafelS <safel_safel@outlook.de>
2026-03-27 17:11:55 +0300

    feat: add JwtService
    - generateToken: creates JWT token with email as subject and expiration date
    - extractEmail: extracts email from JWT token
    - isTokenValid: validates token by checking email match and expiration date

---
+++ src/main/java/com/project/ecommerce/service/JwtService.java
@@ -0,0 +1,62 @@
+package com.project.ecommerce.service;
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload().getSubject();
+
+    }
+    
+    public boolean isTokenValid(String token,  UserDetails userDetails) {
+        String userEmail = extractEmail(token);
+        Date now = new Date();
+        boolean isNotExpired = Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getPayload()
+                .getExpiration()
+                .after(now);
+        boolean emailIsValid = userEmail.equals(userDetails.getUsername());
+        return isNotExpired && emailIsValid;
+    }
+
+
+
+}
